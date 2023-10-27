485bb0068d2e9724a5ba7dc27aa3bcf11a18d408
Nadin Chernova <nchernova2702@gmail.com>
2023-10-26 17:09:45 -0700

    added findDuplicate function

--- Recursion_Practice1/main.cpp
+++ Recursion_Practice1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,6 +11,13 @@ int linearSearchR(int [], int, int);
 
 int linearSearchR(int [], int, int, int);
 // linear search recursive - helper function
+
+int findDuplicatesI(string &);
+
+int findDuplicatesR(string &);
+
+int findDuplicatesR(string &, int, int);
+
 
 int main() {
     int SIZE = 6;
@@ -28,6 +36,14 @@ int main() {
     } else {
         cout << "Found at index: " << resultR << endl;
     }
+
+    string str1 = "fgggKlmnOg";
+    int duplicates = findDuplicatesI(str1);
+    cout << "Number of duplicates: " << duplicates << endl;
+
+    string str2 = "naddind";
+    int dup = findDuplicatesR(str2);
+    cout << "Number of duplicates: " << dup << endl;
 
     return 0;
 }
@@ -58,8 +74,32 @@ int linearSearchR(int arr[], int size, int key, int index) {
     if (arr[index] == key) {
         return index;
     }
+
     return linearSearchR(arr, size, key, index + 1);
 }
 
+int findDuplicatesI(string &str) {
+    int count = 0;
+    sort(str.begin(), str.end());
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == str[i + 1])
+            count++;
+    }
+    return count;
+}
 
+int findDuplicatesR(string &str) {
+    sort(str.begin(), str.end());
+    return findDuplicatesR(str, 0, 0);
 
+}
+
+int findDuplicatesR(string &str, int count, int index) {
+    if (index >= str.length()) {
+        return count; // duplicates not found
+    }
+    if (str[index] == str[index + 1]) {
+        count++;
+    }
+    return findDuplicatesR(str, count, index + 1);
+}
